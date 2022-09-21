47a1ed5f983cfb413620b3aeb559862fafe73600
fullmoonfull <highway.go@gmail.com>
2022-09-21 22:31:44 +0900

    整理

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -14,12 +14,24 @@ export default function Introduction() {
             gap={8}
             style={{ maxWidth: '1280px', minHeight: '1000px', margin: 'auto' }}
           >
-            <GridItem colSpan={8} style={{ background: 'blue' }}></GridItem>
-            <Box colSpan={1} />
-            <GridItem colSpan={1} style={{ background: 'green' }}>
+            <GridItem colSpan={5} style={{ background: 'blue' }}>
+              PC用のイメージ画像
+            </GridItem>
+            <GridItem colSpan={5} style={{ background: 'green' }}>
+              PC用の説明
               {/*<Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />*/}
             </GridItem>
           </Grid>
+          <button
+            style={{
+              width: '100px',
+              height: '25px',
+              background: 'black',
+              color: 'white',
+            }}
+          >
+            ボタン
+          </button>
         </ConentBox>
       </>
     );
@@ -33,10 +45,17 @@ export default function Introduction() {
           gap={8}
           style={{ maxWidth: '1280px', minHeight: '1000px', margin: 'auto' }}
         >
-          <GridItem colSpan={1} style={{ background: 'blue' }}></GridItem>
-          <Box colSpan={1} />
-          <GridItem colSpan={8} style={{ background: 'green' }}>
-            {/*<Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />*/}
+          <GridItem colSpan={10} style={{ background: 'blue' }}>
+            SP用のイメージ画像
+          </GridItem>
+        </Grid>
+        <Grid
+          templateColumns="repeat(10, 1fr)"
+          gap={8}
+          style={{ maxWidth: '1280px', minHeight: '1000px', margin: 'auto' }}
+        >
+          <GridItem colSpan={10} style={{ background: 'green' }}>
+            SP用の説明
           </GridItem>
         </Grid>
       </ConentBox>
